e280f8b4efa1cb07a16dbd79d66324137b1ef8ae
Your Name <you@example.com>
2023-04-16 12:43:15 -0400

    first element

---
+++ my-react-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+let firstElement = <h1>Hello world!!!</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(firstElement);
